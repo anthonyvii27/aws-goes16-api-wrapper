3413bcdd5be7cc5d3be297722816828338ec7d4a
Anthony Vinicius <anthony.vii27@gmail.com>
2022-06-18 20:38:28 -0300

    update: updates in example file

--- example.py
+++ example.py
@@ -1,18 +1,19 @@
 from awsS3ApiGoes16 import AwsS3ApiGoes16
 
 apiGoes = AwsS3ApiGoes16()
-
 apiGoes.product = 'GLM-L2-LCFA'
 
 print(f'Remote bucket: {apiGoes.remote_bucket}')
 print(f'Local bucket: {apiGoes.local_bucket}')
 print(f'Product: {apiGoes.product}')
 
-print('\n\n')
+apiGoes.initial_date = '2019-04-08'
+apiGoes.due_date = '2019-05-08'
 
-bucket_list = apiGoes.s3_client.list_buckets(apiGoes.remote_bucket, apiGoes.local_bucket)
+print(f'Initial date: {apiGoes.initial_date}')
+print(f'Due date: {apiGoes.due_date}')
 
-print('\n\n')
+print('\n')
 
 apiGoes.local_bucket = '/Users/testing'
 print(f'New local bucket: {apiGoes.local_bucket}')
@@ -20,6 +21,10 @@ print(f'New local bucket: {apiGoes.local_bucket}')
 apiGoes.local_bucket = '/'
 print(f'New local bucket from alias: {apiGoes.local_bucket}')
 
-file_list = apiGoes.s3_client.list_files('noaa-goes16/GLM-L2-LCFA/2019/098/18', apiGoes.local_bucket)
+print('\n')
 
-# apiGoes.s3_client.get_file('noaa-goes16/GLM-L2-LCFA/2019/098/18/OR_GLM-L2-LCFA_G16_s20190981800000_e20190981800200_c20190981800229.nc', 'OR_GLM-L2-LCFA_G16_s20190981800000_e20190981800200_c20190981800229.nc')
+bucket_list = apiGoes.s3_client.list_buckets(apiGoes.remote_bucket, apiGoes.local_bucket)
+
+print('\n')
+
+file_list = apiGoes.s3_client.list_files('noaa-goes16/GLM-L2-LCFA/2019/098/18', apiGoes.local_bucket)
