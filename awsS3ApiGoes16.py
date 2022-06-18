594aa74a6d32aa1222f259f7563ca54bf378032b
Anthony Vinicius <anthony.vii27@gmail.com>
2022-06-18 20:34:01 -0300

    feat: add validations

--- awsS3ApiGoes16.py
+++ awsS3ApiGoes16.py
@@ -1,4 +1,5 @@
 from awsS3Api import AwsS3Api
+from utils import convert_to_date
 import re
 
 
@@ -35,14 +36,23 @@ class AwsS3ApiGoes16(AwsS3Api):
         is_valid = re.match(r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$', date)
         if not is_valid:
             raise Warning('Invalid format')
-        self.__initial_date = date
+        self.__initial_date = convert_to_date(date)
 
     @due_date.setter
     def due_date(self, date):
         is_valid = re.match(r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$', date)
         if not is_valid:
             raise Warning('Invalid format')
-        self.__due_date = date
+
+        formatted_date = convert_to_date(date)
+
+        if not self.__initial_date:
+            self.__due_date = formatted_date
+        else:
+            if self.__initial_date > formatted_date:
+                raise Warning('The due date entered is earlier than the initial date')
+            else:
+                self.__due_date = formatted_date
 
     @data_variable.setter
     def data_variable(self, value):
